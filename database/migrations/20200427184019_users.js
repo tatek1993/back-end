ae451f7fea82ebf6f028e19e16afc6f1aa6634ec
Tate Keller <tatek1993@gmail.com>
2020-04-28 20:04:23 -0500

    tables refactored, Created file for raw seed data for strains

--- database/migrations/20200427184019_users.js
+++ database/migrations/20200427184019_users.js
@@ -1,27 +1,92 @@
 
 exports.up = function(knex) {
-  return knex.schema.createTable('users', users => {
-    users
-        .increments();
-
-    users
-        .string('username', 128)
-        .notNullable()
-        .unique();
-
-    users
-        .string('password', 128)
-        .notNullable();
-
-    users
-        .boolean('over18')
-        .notNullable();
-        
-    users
-        .string('zipcode', 9)  
+  return knex.schema.createTable('users', tbl => {
+    tbl
+      .increments();
+    tbl
+      .string('username', 128)
+      .notNullable()
+      .unique();
+    tbl
+      .string('password', 128)
+      .notNullable();
+    tbl
+      .boolean('over18')
+      .notNullable();
+    tbl
+      .string('zipcode', 9)  
   })
+
+  .createTable('searches', tbl => {
+    tbl
+      .increments();
+    tbl
+      .integer('user_id')
+      .unsigned()
+      .notNullable()
+      .references('id')
+      .inTable('users')
+      .onUpdate('CASCADE')
+      .onDelete('CASCADE')
+    tbl
+      .string('effect')
+      .notNullable();
+    tbl
+      .string('flavor')
+      .notNullable();
+    tbl
+      .string('symptoms')
+      .notNullable();
+  })
+  
+  .createTable('strains', tbl => {
+    tbl
+      .string('strain')
+      .notNullable()
+      .primary();
+    tbl
+      .float('rating', 5)
+      .notNullable();
+    tbl
+      .string('flavor')
+      .notNullable();
+    tbl
+      .string('effects')
+      .notNullable();
+    tbl
+      .text('description', 1500)
+      .notNullable();
+  })
+
+  .createTable('results', tbl => {
+    tbl
+      .integer('search_id')
+      .unsigned()
+      .notNullable()
+      .references('id')
+      .inTable('searches')
+      .onUpdate('CASCADE')
+      .onDelete('CASCADE')
+    tbl
+      .integer('result_number', 5)
+      .notNullable()
+      .unsigned()
+    tbl
+      .string('strain_name')
+      .notNullable()
+      .references('strain')
+      .inTable('strains')
+      .onUpdate('CASCADE')
+      .onDelete('CASCADE')
+      
+  })
+
 };
 
 exports.down = function(knex) {
-  return knex.schema.dropTableIfExists('users');
+  return knex.schema
+    .dropTableIfExists('results')
+    .dropTableIfExists('strains')
+    .dropTableIfExists('searches')
+    .dropTableIfExists('users')
 };
