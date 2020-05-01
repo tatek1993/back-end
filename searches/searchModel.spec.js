87539ba001d2209721810c36c2d715682d3af405
Tate Keller <tatek1993@gmail.com>
2020-04-30 21:31:54 -0500

    Automatically added user id to create and put request bodies

--- searches/searchModel.spec.js
+++ searches/searchModel.spec.js
@@ -32,34 +32,67 @@ describe('GET /api/searches', function() {
     });
 })
 
-// describe('authRouter.js', function() {
-//     describe('PUT /:id', function() {
-//         it('should return 200 updated', function() {
-//             return request(server)
-//                 .put('/api/searches/:id')
-//                 .send({
-//                     "user_id": 1,
-//                     "effect": [
-//                         "Tingly",
-//                         "Aroused"
-//                     ],
-//                     "flavor": [
-//                         "Minty",
-//                         "Pine"
-//                     ],
-//                     "symptoms": [
-//                         "Lack of Appetite",
-//                         "Happy",
-//                         "Giggly"
-//                     ],
-//                     "results": [
-//                         "Island-Haze",
-//                         "Batgirl",
-//                         "Sinai",
-//                         "Starbud",
-//                         "Tesla"
-//                     ]
+describe('POST /api/searches', function() {
+    it('should create a new search', function() {
+        return request(server)
+            .post('/api/auth/login')
+            .send({
+                username: 'testlogin1',
+                password: 'testing123'
+            })
+            .then(res => {
+                return request(server)
+                .post 
+            })
+    })
+})
+
+// describe('/api/searches/:id', function() {
+//     it('should return a search with the given search id', function() {
+//         return request(server)
+//             .post('/api/auth/login')
+//             .send({
+//                 username: 'testlogin1',
+//                 password: 'testing123'
+//             })
+//             .then( res => {
+//                 return request(server)
+//                 .get('/api/searches/:id')
+//                 .set('Authorization', res.body.token )
+//                 .then( res => {
+//                     expect(res.status).toBe(200);
 //                 })
-//         })
+//             });
 //     })
 // })
+// // describe('authRouter.js', function() {
+// //     describe('PUT /:id', function() {
+// //         it('should return 200 updated', function() {
+// //             return request(server)
+// //                 .put('/api/searches/:id')
+// //                 .send({
+// //                     "user_id": 1,
+// //                     "effect": [
+// //                         "Tingly",
+// //                         "Aroused"
+// //                     ],
+// //                     "flavor": [
+// //                         "Minty",
+// //                         "Pine"
+// //                     ],
+// //                     "symptoms": [
+// //                         "Lack of Appetite",
+// //                         "Happy",
+// //                         "Giggly"
+// //                     ],
+// //                     "results": [
+// //                         "Island-Haze",
+// //                         "Batgirl",
+// //                         "Sinai",
+// //                         "Starbud",
+// //                         "Tesla"
+// //                     ]
+// //                 })
+// //         })
+// //     })
+// // })
--- searches/searchRouter.js
+++ searches/searchRouter.js
@@ -5,6 +5,7 @@ const db = require('../database/dbConfig.js');
 
 router.post('/', (req, res) => {
     const searchData = req.body;
+    searchData.user_id = req.decodedToken.subject;
 
     Search.add(searchData)
         .then(search => {
@@ -48,6 +49,8 @@ router.get('/', (req, res) => {
 router.put('/:id', (req, res) => {
     const updatedSearch = req.body;
     updatedSearch.id = req.params.id;
+    updatedSearch.user_id = req.decodedToken.subject;
+    
     if (updatedSearch.results.length ==! 5) {
         console.log('There was a problem with your list of results', req.params.id, updatedSearch);
         res.status(400).json({ errorMessage: 'There must be 5 strain results.'})
